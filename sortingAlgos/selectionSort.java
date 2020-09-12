089064f19e21afe87e2d1e960d5550060294307c
Nivetha <nivetha@sap.com>
2020-09-12 12:30:00 +0530

    selection sort code added

--- sortingAlgos/selectionSort.java
+++ sortingAlgos/selectionSort.java
@@ -1,5 +1,30 @@
 package sortingAlgos;
 
-public class selectionSort {
+import java.util.Arrays;
 
+public class selectionSort {
+	
+	public static void main(String args[]) {
+		int arr[] = {5, 3, 8, 2};
+		int n = arr.length;
+		int min = arr[0];
+		
+		for(int i=0; i<n-1; i++) {
+			int minIndex = i;
+			for(int j=i+1; j<n; j++) {
+				if(arr[j]<arr[i]) {
+					minIndex = j;
+					min = arr[minIndex];
+				}
+			}
+			
+			if(i!=minIndex) {
+				arr[minIndex] = arr[i];
+				arr[i] = min;
+				
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
 }
